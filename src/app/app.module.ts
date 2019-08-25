b316ea5238ac21584a0a61fab05bf7522431a2ee
Wayne Hite <waynehite@Waynes-iMac.local>
2019-08-25 19:07:21 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyTestComponent } from './my-test/my-test.component';
+import { TestContentService } from './test-content.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [TestContentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
